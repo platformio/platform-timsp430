3763b736fdae64499592d569a123d9d9257834db
Ivan Kravets <me@ikravets.com>
2016-08-03 20:10:01 +0300

    Refactor DevPlatform to PioPlatform

--- builder/frameworks/energia.py
+++ builder/frameworks/energia.py
@@ -29,7 +29,7 @@ from os.path import isdir, join
 from SCons.Script import DefaultEnvironment
 
 env = DefaultEnvironment()
-platform = env.DevPlatform()
+platform = env.PioPlatform()
 
 FRAMEWORK_DIR = platform.get_package_dir("framework-energiamsp430")
 FRAMEWORK_VERSION = platform.get_package_version("framework-energiamsp430")
