89171ada05b2f6d1260c9420b951f257c8271df3
Ivan Kravets <me@ikravets.com>
2016-08-30 21:49:56 +0300

    Remove unused 'test' target

--- builder/main.py
+++ builder/main.py
@@ -125,13 +125,7 @@ upload = env.Alias(["upload", "uploadlazy"], target_firm,
 AlwaysBuild(upload)
 
 #
-# Target: Unit Testing
-#
-
-AlwaysBuild(env.Alias("test", [target_firm, target_size]))
-
-#
-# Target: Define targets
+# Default targets
 #
 
 Default([target_firm, target_size])
